9dc353b547107eac12d76372c7eac9b82990dbbb
Romu Chollet <chollet@novius.com>
2018-06-18 09:50:55 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ Event::register_function('front.display', function (&$html) {
     $config = \Config::load('novius_tarteaucitron::config', false, true);
     $code = '<script type="text/javascript">tarteaucitron.lang = '. json_encode(\Arr::get($config, 'translations', [])) .'</script>';
 
-    //make code at the and of the script
+    //make code at the end of the script
     if (!empty($code) && preg_match('/<\/body>/i', $html, $matches)) {
         $html = str_replace($matches[0], "\n\n".$code."\n\n".$matches[0], $html);
     }
