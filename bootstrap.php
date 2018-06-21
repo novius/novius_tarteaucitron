8333022217f1b37f0d66d5fab5a60c0c9a0e2614
Cyril Garsaud <garsaud@novius.fr>
2018-06-21 15:37:33 +0200

    fixes

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * Event triggered before cache is written
- */
-Event::register_function('front.display', function (&$html) {
-    $config = \Config::load('novius_tarteaucitron::config', false, true);
-    $code = '<script type="text/javascript">tarteaucitron.lang = '. json_encode(\Arr::get($config, 'translations', [])) .'</script>';
-
-    //make code at the end of the script
-    if (!empty($code) && preg_match('/<\/body>/i', $html, $matches)) {
-        $html = str_replace($matches[0], "\n\n".$code."\n\n".$matches[0], $html);
-    }
-});
 
 Event::register('front.pageFound', function($params)
 {
--- views/script.php
+++ views/script.php
@@ -2,6 +2,8 @@
         src="<?= \Arr::get($config, 'script') ?>"></script>
 
 <script type="text/javascript">
+    tarteaucitron.lang =
+        <?= json_encode(\Arr::get($config, 'translations', [])) ?>;
     tarteaucitron.init(
         <?= json_encode(\Arr::get($config, 'settings', [])) ?>
     );
