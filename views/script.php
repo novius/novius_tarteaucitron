2286286e12f433d6e14ecaf375e470ecee4a5ed5
Cyril <cyril.garsaud@gmail.com>
2018-12-11 18:53:45 +0100

    Google services automatic fallback (#7)
    * Translations ! (#3)
    
    * translation app
    
    * Update bootstrap.php
    
    * Update config.php
    
    * Update config.php
    
    * multi lang
    
    * comment delete
    
    * code style
    
    * Add Hotjar service (#4)
    
    * add hotjar service
    
    * andline
    
    * Google Services: automatic fallback

--- views/script.php
+++ views/script.php
@@ -1,10 +1,12 @@
-<script type="text/javascript"
-        src="<?= \Arr::get($config, 'script') ?>"></script>
+<?php if ($js = \Arr::get($config, 'script')): ?>
+    <script type="text/javascript"
+            src="<?= $js.'?'.md5_file($js) ?>"></script>
+<?php endif ?>
 
 <script type="text/javascript">
     tarteaucitron.lang =
-        <?= json_encode(\Arr::get($config, 'translations', [])) ?>;
+        <?= json_encode(\Arr::get($config, 'translations', array())) ?>;
     tarteaucitron.init(
-        <?= json_encode(\Arr::get($config, 'settings', [])) ?>
+        <?= json_encode(\Arr::get($config, 'settings', array())) ?>
     );
 </script>
