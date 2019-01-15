23c75ba4c78c53d253fc76f5cc4ec3d46f515a7f
Cyril Garsaud <garsaud@novius.fr>
2019-01-15 15:34:45 +0100

    Google Services: Special fallback for GTM

--- static/tarteaucitron.services.js
+++ static/tarteaucitron.services.js
@@ -1059,6 +1059,38 @@ tarteaucitron.services.googletagmanager = {
     }
 };
 
+// google tag manager with fallback
+tarteaucitron.services.googletagmanagerwithfallback = {
+    "key": "googletagmanagerwithfallback",
+    "type": "api",
+    "name": "Google Tag Manager",
+    "uri": "http://www.google.com/ads/preferences/",
+    "needConsent": true,
+    "cookies": ['_ga', '_gat', '__utma', '__utmb', '__utmc', '__utmt', '__utmz', '__gads', '_drt_', 'FLC', 'exchange_uid', 'id', 'fc', 'rrs', 'rds', 'rv', 'uid', 'UIDR', 'UID', 'clid', 'ipinfo', 'acs'],
+    "js": function () {
+        "use strict";
+        if (tarteaucitron.user.googletagmanagerwithfallbackId === undefined) {
+            return;
+        }
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js',
+            'rgpd': 1
+        });
+        tarteaucitron.addScript('//www.googletagmanager.com/gtm.js?id=' + tarteaucitron.user.googletagmanagerwithfallbackId);
+    },
+    "fallback": function () {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js',
+            'rgpd': 0
+        });
+        tarteaucitron.addScript('//www.googletagmanager.com/gtm.js?id=' + tarteaucitron.user.googletagmanagerwithfallbackId);
+    }
+};
+
 // jsapi
 tarteaucitron.services.jsapi = {
     "key": "jsapi",
