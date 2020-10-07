d453c119885c0e634a49a33d791a2bbca481b407
Kévin Legrand <manoz@outlook.com>
2020-10-07 15:30:59 +0200

    [Update] TAC Scripts v1.0.4 (#11)
    * Fix BingAds service
    * Fix an xss vulnerability
    * Fix privacy page button
    * Add LinkedIn Tag service
    
    Co-authored-by: Kévin Legrand <legrand@novius.fr>

--- config/config.php
+++ config/config.php
@@ -22,7 +22,7 @@ return array(
         'privacyUrl' => '',
         'useExternalCss' => false,
         'useExternalJs' => false,
-        'mandatory' => false,
+        'mandatory' => true,
     ),
 
     'translations' => array(
