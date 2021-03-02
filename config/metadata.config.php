a4dcca21bf1bf3c01450fb30c86eaf2f03f0dd8a
Manoz <manoz@outlook.com>
2021-03-02 14:16:23 +0100

    Bugfix - php 5.3 compatibility

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -13,7 +13,7 @@ if (\Str::starts_with(\Arr::get($version, 'fullname'), 'Chiba')) {
 
 return array(
     'name' => 'Tarte Au Citron',
-    'version' => '2.1.3',
+    'version' => '2.1.4',
     'provider' => array(
         'name' => 'Novius',
     ),
